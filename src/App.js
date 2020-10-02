97668ddcaa7fbdce43786aa0f9db6369f43b1f5a
Chintan Savalia <chintan2011@gmail.com>
2020-10-02 19:11:12 -0400

    showing workexperience by default

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
     super();
     this.state = {
       name: "React",
-      showHideWorkExperience: false,
+      showHideWorkExperience: true,
       showHideProjects: false,
       showInternship: false,
       showEducation: false,
