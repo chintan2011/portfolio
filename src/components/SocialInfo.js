c0061c760b7fca6f18d6a0069ee4feffd9f681d8
Chintan Savalia <chintan2011@gmail.com>
2020-11-10 17:02:30 -0500

    added support for mobile view

--- src/components/SocialInfo.js
+++ src/components/SocialInfo.js
@@ -8,7 +8,8 @@ function SocialInfo() {
       <div class="contactinfo">
         <SocialIcon url="https://www.linkedin.com/in/chintan-savalia/" />
         <SocialIcon url="https://github.com/chintan2011" bgColor="#00000" />
-        <SocialIcon url="https://www.facebook.com/chintan2011" />
+        {/* <SocialIcon url="https://www.facebook.com/chintan2011" /> */}
+        <SocialIcon url="https://learnwithchintan.web.app/" bgColor="#1197D5" network="google_play"/>
       </div>
     </div>
   )
